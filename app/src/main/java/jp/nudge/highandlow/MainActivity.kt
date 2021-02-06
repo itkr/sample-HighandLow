6bd0a83383947a89a504b9db493b3a600c9d67e3
Shota Itakura <1531860+itkr@users.noreply.github.com>
2021-02-06 23:48:54 +0900

    Update methods

--- app/src/main/java/jp/nudge/highandlow/MainActivity.kt
+++ app/src/main/java/jp/nudge/highandlow/MainActivity.kt
@@ -14,27 +14,12 @@ class MainActivity : AppCompatActivity() {
     private var gameStart = false
     private var answered = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        hitCount = 0
-        loseCount = 0
-        hitText.text = getString(R.string.hit_text)
-        loseText.text = getString(R.string.lose_text)
-        gameStart = true
-        drawCard()
-    }
-
     private fun drawCard() {
         yourCardImage.setImageResource(R.drawable.z02)
         droidCardImage.setImageResource(R.drawable.z01)
 
         //乱数
-        yourCard = (1..3).random()
+        yourCard = (1..13).random()
         Log.d(tag, "you:" + yourCard)
         when(yourCard) {
             1 -> yourCardImage.setImageResource(R.drawable.d01)
@@ -51,8 +36,86 @@ class MainActivity : AppCompatActivity() {
             12 -> yourCardImage.setImageResource(R.drawable.d12)
             13 -> yourCardImage.setImageResource(R.drawable.d13)
         }
-        droidCard = (1..3).random()
+        droidCard = (1..13).random()
         Log.d(tag, "you:"+ droidCard)
         answered = false
+    }
+
+    private fun highAndLow(answer: Char) {
+        showDroidCard()
+        answered = true
+        val balance = droidCard - yourCard
+        if (balance == 0) {
+            // when even do noting
+        } else if ((balance > 0 && answer == 'h')) {
+            hitCount++
+            hitText.text = getString(R.string.hit_text) + hitCount
+        } else if ((balance < 0 && answer == 'l')) {
+            hitCount++
+            hitText.text = getString(R.string.hit_text) + hitCount
+        } else {
+            loseCount++
+            loseText.text = getString((R.string.lose_text)) + loseCount
+        }
+        if (hitCount == 5) {
+            resultText.text = "You Win!"
+            gameStart = false
+        } else if (loseCount == 5) {
+            resultText.text = "You Lose!"
+            gameStart = false
+        } else {
+            // do noting
+        }
+    }
+
+    private fun showDroidCard(){
+       when (droidCard) {
+           1 -> droidCardImage.setImageResource(R.drawable.c01)
+           2 -> droidCardImage.setImageResource(R.drawable.c02)
+           3 -> droidCardImage.setImageResource(R.drawable.c03)
+           4 -> droidCardImage.setImageResource(R.drawable.c04)
+           5 -> droidCardImage.setImageResource(R.drawable.c05)
+           6 -> droidCardImage.setImageResource(R.drawable.c06)
+           7 -> droidCardImage.setImageResource(R.drawable.c07)
+           8 -> droidCardImage.setImageResource(R.drawable.c08)
+           9 -> droidCardImage.setImageResource(R.drawable.c09)
+           10 -> droidCardImage.setImageResource(R.drawable.c10)
+           11 -> droidCardImage.setImageResource(R.drawable.c11)
+           12 -> droidCardImage.setImageResource(R.drawable.c12)
+           13 -> droidCardImage.setImageResource(R.drawable.c13)
+       }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        highBtn.setOnClickListener {
+            if ((gameStart && !answered)) {
+                highAndLow('h')
+            }
+        }
+
+        lowBtn.setOnClickListener {
+            if ((gameStart && !answered)) {
+                highAndLow('l')
+            }
+        }
+
+        nextBtn.setOnClickListener {
+            if ((gameStart)) {
+                drawCard()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hitCount = 0
+        loseCount = 0
+        hitText.text = getString(R.string.hit_text)
+        loseText.text = getString(R.string.lose_text)
+        gameStart = true
+        drawCard()
     }
 }
